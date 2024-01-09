b024f38e42d03b5934c4fd2f67ff349f1f46b72f
mehedi-hasan <mehediinform@gmail.com>
2024-01-10 04:03:58 +0600

    2nd Commit Zoomcontrols

--- app/src/main/java/com/example/zoomcontrols/MainActivity.java
+++ app/src/main/java/com/example/zoomcontrols/MainActivity.java
@@ -3,12 +3,53 @@ package com.example.zoomcontrols;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ZoomControls;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+    private ZoomControls zoomControls;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView_Id);
+        zoomControls = findViewById(R.id.zoomControls_Id);
+
+        zoomControls.setOnZoomInClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float x = imageView.getScaleX();
+                float y = imageView.getScaleY();
+
+                if (x<7 && y<7){
+
+                    imageView.setScaleX((float) x+1);
+                    imageView.setScaleY((float) y+1);
+                }
+
+
+            }
+        });
+
+        zoomControls.setOnZoomOutClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float x = imageView.getScaleX();
+                float y = imageView.getScaleY();
+
+                if (x>1 && y>1){
+
+                    imageView.setScaleX((float) x-1);
+                    imageView.setScaleY((float) y-1);
+                }
+
+            }
+        });
+
     }
 }
